aed9c44fc9e20b4f9cfd789f79028770c131eb34
Helen Ma <hhma@alumni.cpp.edu>
2023-11-05 11:09:48 -0500

    use trusted package configs.

--- src/main/java/dev/lydtech/dispatch/handler/DispatchTrackingHandler.java
+++ src/main/java/dev/lydtech/dispatch/handler/DispatchTrackingHandler.java
@@ -4,24 +4,26 @@ import dev.lydtech.dispatch.message.DispatchPreparing;
 import dev.lydtech.dispatch.service.TrackingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@KafkaListener(
+        id = "orderDispatchClient",
+        topics = "dispatch.tracking",
+        groupId = "dispatch.order.dispatch.consumer",
+        containerFactory = "kafkaListenerContainerFactory"
+)
 public class DispatchTrackingHandler {
 
 
     private final TrackingService trackingService ;
 
 
-    @KafkaListener(
-            id = "orderDispatchClient",
-            topics = "dispatch.tracking",
-            groupId = "dispatch.order.dispatch.consumer",
-            containerFactory = "kafkaListenerContainerFactory"
-    )
+    @KafkaHandler
     public void listen(DispatchPreparing dispatchPreparing)   {
 
         log.info("Received message: payload: " + dispatchPreparing);
